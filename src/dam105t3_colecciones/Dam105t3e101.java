b0f4a35da69ba8eab0cb00fb63dae5fcbf7b3c8e
MrDev-12 <micorreoelectronico258@gmail.com>
2020-04-26 14:33:49 +0200

    First Upload - 26/04/2020

---
+++ src/dam105t3_colecciones/Dam105t3e101.java
@@ -0,0 +1,21 @@
+
+package dam105t3_colecciones;
+
+import java.util.Scanner;
+
+
+public class Dam105t3e101 {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduce un ancho: ");
+        double ancho = teclado.nextDouble();
+
+        System.out.print("Introduce un alto: ");
+        double alto = teclado.nextDouble();
+
+    }
+    
+}
